89b7bdc7b340e00e4c8213e23e859228d89d7e72
noga <198326931+Noga-ng@users.noreply.github.com>
2026-07-28 18:05:18 +0300

    2em amelioration

--- Tests/Testsc.php
+++ Tests/Testsc.php
@@ -1,11 +1,53 @@
 <?php
 require __DIR__."/../vendor/autoload.php";
 
+use NCache\Config\Ttl\Duration;
 use NCache\Enum\CType;
 use NCache\NCache;
 
 NCache::config(__DIR__ . '/../cache');
 
-$clear = NCache::clear(CType::JSON);
+$item = NCache::key('users', CType::JSON)
+    ->dir('json')
+    ->set([
+        'id' => 1,
+        'name' => 'Noga',
+    ])
+    ->set([
+        'id' => 2,
+        'name' => 'Germainio',
+    ])
+    ->set([
+        'id' => 3,
+        'name' => 'John',
+    ])
+    ->ttl(Duration::days(1))
+    ->signature('users');
 
-var_dump($clear);
+$item->put();
+
+echo json_encode($item->show(), JSON_PRETTY_PRINT);
+
+// result 
+// {
+//     "type": "JSON",
+//     "name": "users",
+//     "key": "48860f98be550e0ac7167fdc4036a2ff",
+//     "signature": "7dfb4cf67742cb0660305e56ef816c53fcec892cae7f6ee39b75f34e659d672c",
+//     "ttl": 86400,
+//     "expiresAt": 1785337430,
+//     "data": [
+//         {
+//             "id": 1,
+//             "name": "Noga"
+//         },
+//         {
+//             "id": 2,
+//             "name": "Germainio"
+//         },
+//         {
+//             "id": 3,
+//             "name": "John"
+//         }
+//     ]
+// }
